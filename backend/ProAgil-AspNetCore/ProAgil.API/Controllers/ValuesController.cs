3eef36c3c4b900558fc64a018e98591e0d1cd76c
RYOJI KITANO NETO <90005072@pmenos.com.br>
2021-05-24 21:43:08 -0300

    init orgnizing directorys create frontend in angular

---
+++ backend/ProAgil-AspNetCore/ProAgil.API/Controllers/ValuesController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProAgil.API.Data;
+using ProAgil.API.Model;
+
+namespace ProAgil.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+      private readonly DataContext _context;
+      public ValuesController(DataContext context)
+      {
+          _context = context;
+      }
+        // GET api/values
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Evento>>> Get()
+        {
+            var result = await _context.Eventos.ToListAsync();
+            try
+            {
+              return Ok(result);
+            }
+            catch(System.Exception){
+              return this.StatusCode(StatusCodes.Status500InternalServerError, "Erro ao encontrar");
+            }
+            
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<Evento> Get(int id)
+        {
+            return _context.Eventos.FirstOrDefault(p => p.EventoId == id);
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
